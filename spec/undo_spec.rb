0e79531a0b4edc44f68c221f50503104f309813d
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-02-16 17:17:23 +0200

    Fix nesting in specs

--- spec/undo_spec.rb
+++ spec/undo_spec.rb
@@ -1,11 +1,10 @@
 require "spec_helper_lite"
 
 describe Undo do
+  let(:object) { double :object, change: true }
   subject { described_class }
 
   describe "#wrap" do
-    let(:object) { double :object, change: true }
-
     before do
       subject.configure do |config|
         config.mutator_methods = [:change]
@@ -22,26 +21,26 @@ describe Undo do
       expect(decorator.class).to eq Array
       expect(decorator).to be_a Array
     end
+  end
 
-    describe "restores object by uuid" do
-      it "restores object" do
-        model = subject.wrap object
-        model.change
-        restored_object = subject.restore model.uuid
+  describe "restores object by uuid" do
+    it "restores object" do
+      model = subject.wrap object
+      model.change
+      restored_object = subject.restore model.uuid
 
-        expect(restored_object).to eq object
-      end
+      expect(restored_object).to eq object
+    end
 
-      it "restores using provided options" do
-        serializer = double :serializer
-        expect(serializer).to receive(:deserialize) { object }
+    it "restores using provided options" do
+      serializer = double :serializer
+      expect(serializer).to receive(:deserialize) { object }
 
-        model = subject.wrap object
-        model.change
-        restored_object = subject.restore model.uuid, serializer: serializer
+      model = subject.wrap object
+      model.change
+      restored_object = subject.restore model.uuid, serializer: serializer
 
-        expect(restored_object).to eq object
-      end
+      expect(restored_object).to eq object
     end
   end
 end
