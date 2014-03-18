a1e4cc464b4353588ae903a84b87c24cecec9c93
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-03-18 15:38:19 +0200

    Add spec/support/ci_helper

--- spec/spec_helper_lite.rb
+++ spec/spec_helper_lite.rb
@@ -1,12 +1,5 @@
-if !!ENV['CI']
-  require 'coveralls'
-  Coveralls.wear!
-else
-  require 'pry'
-end
-
-ENV['RAILS_ENV'] ||= 'test'
 require 'rspec'
+require_relative "support/ci_helper"
 require 'undo'
 
 $: << File.expand_path('../lib', File.dirname(__FILE__))
