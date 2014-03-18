fa1f2ed9c07963e9185ee7b108b525e9da46fb63
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-03-19 01:40:22 +0200

    Specs cleanup

--- spec/undo/serializer/null_spec.rb
+++ spec/undo/serializer/null_spec.rb
@@ -2,18 +2,15 @@ require "spec_helper_lite"
 require "undo/serializer/null"
 
 describe Undo::Serializer::Null do
-  subject { described_class }
-  let(:serializer) { described_class.new }
-
   describe "returns passed argument" do
     let(:object) { double :object }
 
     specify "#serialize" do
-      expect(serializer.serialize object).to eq object
+      expect(subject.serialize object).to eq object
     end
 
     specify "#deserialize" do
-      expect(serializer.deserialize object).to eq object
+      expect(subject.deserialize object).to eq object
     end
   end
 end
--- spec/undo/storage/memory_spec.rb
+++ spec/undo/storage/memory_spec.rb
@@ -2,18 +2,16 @@ require "spec_helper_lite"
 require 'undo/storage/memory'
 
 describe Undo::Storage::Memory do
-  subject { described_class }
-  let(:adapter) { subject.new }
   let(:object) { double :object }
 
   it "stores any object" do
-    adapter.store 123, object
-    expect(adapter.fetch 123).to eq object
+    subject.store 123, object
+    expect(subject.fetch 123).to eq object
   end
 
   it "deletes stored object" do
-    adapter.store 123, object
-    adapter.delete 123
-    expect { adapter.fetch 123 }.to raise_error(KeyError)
+    subject.store 123, object
+    subject.delete 123
+    expect { subject.fetch 123 }.to raise_error(KeyError)
   end
 end
